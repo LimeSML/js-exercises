84384adbecfc52e3f6552d9e066f380200b2d4b1
LimeSML <raimuhoshi@DESKTOP-N8HGJRS.>
2025-04-11 21:07:39 +0900

    ch02/ex08 を解きなおした

--- ch02/ex08/index.js
+++ ch02/ex08/index.js
@@ -1,53 +1,15 @@
-import { parse, tokenizer } from "acorn";
+import { parse } from "acorn";
 
-function stripPositions(node) {
-    if (Array.isArray(node)) {
-        return node.map(stripPositions);
-    } else if (node && typeof node === "object") {
-        const newNode = {};
-        for (const key in node) {
-            if (key === "start" || key === "end") {
-                continue;
-            }
-            newNode[key] = stripPositions(node[key]);
-        }
-        return newNode;
-    }
-    return node;
-}
+const codeWithoutSemicolon = `let a
+a
+=
+3
+console.log(a)`;
 
-function getSemicolonIndices(jsCode) {
-    const semicolonIndices = [];
-    for (const token of tokenizer(jsCode, { ecmaVersion: 2020 })) {
-        if (token.type.label === ';') {
-            semicolonIndices.push(token.start);
-        }
-    }
-    return semicolonIndices;
-}
+const codeWithSemicolon = `let a; a = 3; console.log(a);`;
 
-export function removeSemicolon(jsCode) {
-    try {
-        const originalAst = parse(jsCode, { ecmaVersion: 2020 });
-        const semicolonIndices = getSemicolonIndices(jsCode);
+const astWithoutSemicolon = parse(codeWithoutSemicolon, { ecmaVersion: 2020 });
+const astWithSemicolon = parse(codeWithSemicolon, { ecmaVersion: 2020 });
 
-        let outputCode = jsCode;
-        // セミコロンを削除した場合に文字の位置の影響がないよう、逆順に処理する
-        for (let i = semicolonIndices.length - 1; i >= 0; i--) {
-            const start = semicolonIndices[i];
-            const end = start + 1
-            const testCode = outputCode.slice(0, start) + outputCode.slice(end);
-            try {
-                const testAst = parse(testCode, { ecmaVersion: 2020 });
-                if (JSON.stringify(stripPositions(testAst)) === JSON.stringify(stripPositions(originalAst))) {
-                    outputCode = testCode;
-                }
-            } catch (e) {
-                // 特に何もしない
-            }
-        }
-        return outputCode;
-    } catch (e) {
-        return jsCode;
-    }
-}
+console.log(JSON.stringify(astWithoutSemicolon, null , 2));
+console.log(JSON.stringify(astWithSemicolon, null , 2));
